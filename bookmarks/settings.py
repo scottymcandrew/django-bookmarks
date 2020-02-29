d4b31e2dc2f95030d092aeb0f5ecb14972fe0838
Scott McAndrew <scottymcandrew@outlook.com>
2020-02-29 17:45:19 +0000

    Moved static folder back to app, installed whitenoise for Heroku hosting supporting static

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'account/static')
 
 # URL to serve the media files uploaded by users
 MEDIA_URL = '/media/'
